39a5057a3b45045627c9c8c5e613203bfc7f45d1
Markus Nordhaus <markus@theDXPilot.io>
2024-01-03 18:03:36 +0100

    Experimenting with typescript

---
+++ static/admin/admin.js
@@ -0,0 +1,25 @@
+import CMS from '@staticcms/core';
+import '@staticcms/core/dist/main.css';
+CMS.init();
+/** Evemtlistener that reacts on the preSave event in order to reset the weights to the current order in the list
+ The Hugo Navigation gets sorted through the weight field by default. There is no way to have it sorted by the order in the yaml config.
+
+ @see https://gohugo.io/methods/menu/ and https://www.staticcms.org/docs/beta-features#pre-save
+ **/
+/**
+ CMS.registerEventListener({
+    name: 'preSave',
+    collection: 'config',
+    file: 'config/_default/menus.yaml',
+    handler: ({ data: { entry } }: { data: { entry: any } }) => {
+      let n: number = 1;
+      let currentValue: any[] = [...entry.data.main];
+      currentValue.forEach((mainItem: any) => {
+        mainItem.weight = n++;
+      });
+      return {
+        ...entry.data,
+        main: currentValue,
+      };
+    },
+  }); **/ 
